895a71ab6b22468c8ca67184d3813a91ef0348ec
Riki Montgomery <mostriki820@gmail.com>
2017-12-08 15:39:26 -0800

    Cart will update in nav bar when an item is added.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,11 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.new(item_params)
     @order.save
     session[:order_id] = @order.id
-    redirect_to products_path
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
   def update
